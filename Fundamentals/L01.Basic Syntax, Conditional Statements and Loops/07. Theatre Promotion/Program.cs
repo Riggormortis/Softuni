5032b23a7c52b03458a78d48cf69ca01a6eee0d5
Grigor Petrunov <grigor.petrunov@gmail.com>
2022-05-14 22:03:43 +0300

    Commit for first Lab

---
+++ Fundamentals/L01.Basic Syntax, Conditional Statements and Loops/07. Theatre Promotion/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace _07._Theatre_Promotion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string typeOfDay = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+
+            double priceOfTiket = 0;
+
+
+            if (age >= 0 && age <= 18)
+            {
+                if (typeOfDay == "Weekday")
+                {
+                    priceOfTiket = 12;
+                }
+                else if (typeOfDay == "Weekend")
+                {
+                    priceOfTiket = 15;
+                }
+                else if (typeOfDay == "Holiday")
+                {
+                    priceOfTiket = 5;
+                }
+                Console.WriteLine($"{priceOfTiket}$");
+            }
+            else if (age > 18 && age <= 64)
+            {
+                if (typeOfDay == "Weekday")
+                {
+                    priceOfTiket = 18;
+                }
+                else if (typeOfDay == "Weekend")
+                {
+                    priceOfTiket = 20;
+                }
+                else if (typeOfDay == "Holiday")
+                {
+                    priceOfTiket = 12;
+                }
+                Console.WriteLine($"{priceOfTiket}$");
+            }
+            else if (age > 64 && age <= 122)
+            {
+                if (typeOfDay == "Weekday")
+                {
+                    priceOfTiket = 12;
+                }
+                else if (typeOfDay == "Weekend")
+                {
+                    priceOfTiket = 15;
+                }
+                else if (typeOfDay == "Holiday")
+                {
+                    priceOfTiket = 10;
+                }
+                Console.WriteLine($"{priceOfTiket}$");
+            }
+            else
+            {
+                Console.WriteLine("Error!");
+            }
+
+
+        }
+    }
+}
---
+++ Fundamentals/L01.Basic Syntax, Conditional Statements and Loops/09. Sum of Odd Numbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _09._Sum_of_Odd_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            for (int i = 0; i < number; i++)
+            {
+                int currentOdNumber = 1 + (i * 2);
+                // 1 + (0 * 2) = 1
+                // 1 + (1 * 2) = 3
+                // 1 + (2 * 2) = 5
+                sum += currentOdNumber;
+                Console.WriteLine(currentOdNumber);
+            }
+
+            Console.WriteLine($"Sum: {sum}");
+
+        }
+    }
+}
