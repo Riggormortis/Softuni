b809a6c3c1d137601557d5457f364050414ea297
Grigor Petrunov <grigor.petrunov@gmail.com>
2022-05-18 21:10:32 +0300

    Added Solution for 07.

---
+++ C#Advanced/L01.StacksNQueues/07.Hot Potato/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _07.Hot_Potato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] people = Console.ReadLine()
+                .Split();
+
+            Queue<string> queue = new Queue<string>(people);
+            int tossCountToRemove = int.Parse(Console.ReadLine());
+            int currTosses = 1;
+            while (queue.Count > 1)
+            {
+                var kidWithPotato = queue.Dequeue();
+                if (currTosses != tossCountToRemove)
+                {
+                    currTosses++;
+                    queue.Enqueue(kidWithPotato);
+                }
+                else
+                {
+                    currTosses = 1;
+                    Console.WriteLine($"Removed {kidWithPotato}");
+                }
+
+                
+            }
+            Console.WriteLine($"Last is {queue.Dequeue()}");
+        }
+    }
+}
