854652481c0d986171d554d6b892e73dfd1671d4
Grigor Petrunov <grigor.petrunov@gmail.com>
2022-05-25 22:21:42 +0300

    Added Solutions for 3 and 4

---
+++ C#Advanced/L02.Multidimensional Arrays/3. Primary Diagonal/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+namespace _3._Primary_Diagonal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sizeOfMatrix = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[sizeOfMatrix, sizeOfMatrix];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                var currentRow = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = currentRow[col];
+                }
+            }
+
+            int sum = 0;
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int col = row;
+                sum += matrix[row, col];
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
