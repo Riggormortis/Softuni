398c57891fcc380d2bc1890d1d96aec700f4efbf
Grigor Petrunov <grigor.petrunov@gmail.com>
2022-05-25 22:38:44 +0300

    Add Problems to CSharp-Advanced

--- C#Advanced/E02.Multidimensional Arrays Exercise/1. Diagonal Difference/Program.cs
+++ C#Advanced/E02.Multidimensional Arrays Exercise/1. Diagonal Difference/Program.cs
@@ -1,12 +1,51 @@
 ﻿using System;
-
+using System.Linq;
 namespace _1._Diagonal_Difference
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int sizeOfMatrix = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[sizeOfMatrix, sizeOfMatrix];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                var currentRow = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = currentRow[col];
+                }
+            }
+
+            int sumPrime = 0;
+
+            
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int col = row;
+                sumPrime += matrix[row, col];
+            }
+
+            
+            int sumSecondary = 0;
+            int currentCol = 0;
+
+            for (int rowSec = matrix.GetLength(0) - 1; rowSec >= 0; rowSec--)
+            {
+                sumSecondary += matrix[rowSec, currentCol];
+                currentCol++;
+            }
+
+            int difference = sumPrime - sumSecondary;
+
+            Console.WriteLine(Math.Abs(difference));
         }
     }
 }
