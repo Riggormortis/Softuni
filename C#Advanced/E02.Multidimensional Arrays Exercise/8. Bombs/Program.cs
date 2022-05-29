58600679064fe519f2cbca98afe9b69fe25c4941
Grigor Petrunov <grigor.petrunov@gmail.com>
2022-05-29 10:08:32 +0300

    Added solution for 8. Bombs

---
+++ C#Advanced/E02.Multidimensional Arrays Exercise/8. Bombs/Program.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Linq;
+namespace _8._Bombs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sizeOfMatrix = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[sizeOfMatrix, sizeOfMatrix];
+
+            FillMatrix(matrix);
+
+            var coordinates = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            foreach (var coordinate in coordinates)
+            {
+                int[] tokens = coordinate
+                    .Split(",", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                int row = tokens[0];
+                int col = tokens[1];
+
+                if (matrix[row, col] > 0)
+                {
+                    int value = matrix[row, col];
+                    matrix[row, col] -= value;
+
+
+                    if (row - 1 >= 0 && row - 1 < matrix.GetLength(0) &&
+                        col - 1 >= 0 && col - 1 < matrix.GetLength(1))
+                    {
+                        if (matrix[row - 1, col - 1] > 0)
+                        {
+                            matrix[row - 1, col - 1] -= value;
+                        }
+                    }
+                    if (row - 1 >= 0 && row - 1 < matrix.GetLength(0) &&
+                        col >= 0 && col < matrix.GetLength(1))
+                    {
+                        if (matrix[row - 1, col] > 0)
+                        {
+                            matrix[row - 1, col] -= value;
+                        }
+                    }
+                    if (row - 1 >= 0 && row - 1 < matrix.GetLength(0) &&
+                        col + 1 >= 0 && col + 1 < matrix.GetLength(1))
+                    {
+                        if (matrix[row - 1, col + 1] > 0)
+                        {
+                            matrix[row - 1, col + 1] -= value;
+                        }
+                    }
+                    if (row >= 0 && row < matrix.GetLength(0) &&
+                        col - 1 >= 0 && col - 1 < matrix.GetLength(1))
+                    {
+                        if (matrix[row, col - 1] > 0)
+                        {
+                            matrix[row, col - 1] -= value;
+                        }
+                    }
+                    if (row >= 0 && row < matrix.GetLength(0) &&
+                        col + 1 >= 0 && col + 1 < matrix.GetLength(1))
+                    {
+                        if (matrix[row, col + 1] > 0)
+                        {
+                            matrix[row, col + 1] -= value;
+                        }
+                    }
+                    if (row + 1 >= 0 && row + 1 < matrix.GetLength(0) &&
+                        col - 1 >= 0 && col - 1 < matrix.GetLength(1))
+                    {
+                        if (matrix[row + 1, col - 1] > 0)
+                        {
+                            matrix[row + 1, col - 1] -= value;
+                        }
+                    }
+                    if (row + 1 >= 0 && row + 1 < matrix.GetLength(0) &&
+                        col >= 0 && col < matrix.GetLength(1))
+                    {
+                        if (matrix[row + 1, col] > 0)
+                        {
+                            matrix[row + 1, col] -= value;
+                        }
+                    }
+                    if (row + 1 >= 0 && row + 1 < matrix.GetLength(0) &&
+                        col + 1 >= 0 && col + 1 < matrix.GetLength(1))
+                    {
+                        if (matrix[row + 1, col + 1] > 0)
+                        {
+                            matrix[row + 1, col + 1] -= value;
+                        }
+                    }
+                }
+            }
+
+
+            int sum = 0;
+            int activeCellsCount = 0;
+
+            foreach (var element in matrix)
+            {
+                if (element > 0)
+                {
+                    sum += element;
+                    activeCellsCount++;
+                }
+            }
+
+            Console.WriteLine($"Alive cells: {activeCellsCount}");
+            Console.WriteLine($"Sum: {sum}");
+
+            PrintMatrix(matrix);
+        }
+
+        private static void PrintMatrix(int[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write($"{matrix[row, col]} ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+
+        private static void FillMatrix(int[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] currentRow = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = currentRow[col];
+                }
+            }
+        }
+    }
+}
