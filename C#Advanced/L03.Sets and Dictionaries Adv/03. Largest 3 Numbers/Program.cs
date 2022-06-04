a032b92c459278bc612c3330107b4148b1abdce9
Grigor Petrunov <grigor.petrunov@gmail.com>
2022-06-04 11:40:28 +0300

    Add

---
+++ C#Advanced/L03.Sets and Dictionaries Adv/03. Largest 3 Numbers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace _03._Largest_3_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine()
+                  .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                  .Select(int.Parse)
+                  .ToArray();
+
+            int[] sorted = numbers.OrderByDescending(n => n)
+                .ToArray();
+
+            if (sorted.Length >= 3)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    Console.Write($"{sorted[i]} ");
+                }
+            }
+            else
+            {
+                for (int i = 0; i < sorted.Length; i++)
+                {
+                    Console.Write($"{sorted[i]} ");
+                }
+            }
+            
+
+        }
+    }
+}
