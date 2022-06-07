90298fb28414f988eef4e6781a9a0e0f7eb3e73d
Grigor Petrunov <grigor.petrunov@gmail.com>
2022-06-07 17:26:35 +0300

    3.

---
+++ C#Advanced/E05.Funcional Programing/03. Custom Min Function/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace _03._Custom_Min_Function
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            //gets list => returns smallest number in the list
+            Func<List<int>, int> printMinElemet = list =>
+            {
+                int min = int.MaxValue;
+
+                foreach (int number in list)
+                {
+                    if (number < min)
+                    {
+                        min = number;
+                    }
+                }
+
+                return min;
+            };
+
+            printMinElemet(numbers);
+        }
+    }
+}
