8016c1d7994307e6c3d4c564df17fdf5e945e696
Grigor Petrunov <grigor.petrunov@gmail.com>
2022-06-07 20:55:07 +0300

    new adds

---
+++ C#Advanced/E05.Funcional Programing/06. Reverse And Exclude/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace _06._Reverse_And_Exclude
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToList();
+
+            int n = int.Parse(Console.ReadLine());
+
+            Predicate<int> toRemove = number => number % n == 0;
+
+            List<int> newList = new List<int>();
+            List<int> reversedNumbers = Enumerable.Reverse(newList).ToList();
+
+            foreach (int number in numbers)
+            {
+
+                newList.Add(number);
+                
+            }
+
+            
+
+            Console.WriteLine(String.Join(" ", reversedNumbers));
+        }
+    }
+}
---
+++ C#Advanced/E05.Funcional Programing/08. List Of Predicates/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08._List_Of_Predicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<int> dividers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToList();
+
+            List<int> numbers = new List<int>(); //all the numbers from 1 to n
+
+            for (int number = 1; number <= n; number++)
+            {
+                numbers.Add(number);
+            }
+
+            List<int> printNumbers = new List<int>();
+
+            foreach (int number in numbers)
+            {
+                bool isDivisible = true;
+                foreach (int divider in dividers)
+                {
+                    Predicate<int> divisible = number => number % divider == 0;
+
+                    if (!divisible(number))
+                    {
+                        isDivisible = false;
+                        break;
+                    }
+                }
+
+
+                if (isDivisible)
+                {
+                    printNumbers.Add(number);
+                }
+            }
+
+            Console.WriteLine(String.Join(" ", printNumbers));
+
+        }
+    }
+}
