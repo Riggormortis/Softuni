fe62c877e38e60768fe16a4eaa5c3177a54d51f3
Grigor Petrunov <grigor.petrunov@gmail.com>
2022-06-07 18:23:13 +0300

    4. & 5.

---
+++ C#Advanced/E05.Funcional Programing/04. Find Evens or Odds/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace _04._Find_Evens_or_Odds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int[] nums = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            int startNumber = nums[0];
+            int endNumber = nums[1];
+
+            List<int> numbers = new List<int>();
+
+            for (int number = startNumber; number <= endNumber; number++)
+            {
+                numbers.Add(number);
+            }
+            
+            //"odd" or "even"
+            string type = Console.ReadLine();
+
+            Predicate<int> isEven = number => number % 2 == 0; 
+            // true => number is even
+            //false => number is false
+
+            if (type == "even")
+            {
+                foreach (int number in numbers)
+                {
+                    if (isEven(number))
+                    {
+                        Console.Write(number + " ");
+                    }
+                }
+            }
+            else if (type == "odd")
+            {
+                foreach (int number in numbers)
+                {
+                    if (!isEven(number))
+                    {
+                        Console.Write(number + " ");
+                    }
+                }
+            }
+         }
+    }
+}
---
+++ C#Advanced/E05.Funcional Programing/05. Applied Arithmetics/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05._Applied_Arithmetics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            //add
+            Func<List<int>, List<int>> add = list => list.Select(number => number += 1).ToList();
+
+            //multiply
+            Func<List<int>, List<int>> multiply = list => list.Select(number => number *= 2).ToList();
+
+            //subtract
+            Func<List<int>, List<int>> subtract = list => list.Select(number => number -= 1).ToList();
+
+            //print
+            Action<List<int>> print = list => Console.WriteLine(String.Join(" ", list));
+
+            string command = Console.ReadLine();
+
+            while (command != "end")
+            { //comand = "add", "multiply", "substract" or "print"
+
+                switch (command)
+                {
+                    case "add":
+                        numbers = add(numbers);
+                        break;
+
+                    case "multiply":
+                        numbers = multiply(numbers);
+                        break;
+
+                    case "subtract":
+                        numbers = subtract(numbers);
+                        break;
+
+                    case "print":
+                        print(numbers);
+                        break;
+                }
+
+
+                command = Console.ReadLine();
+            }
+        }
+    }
+}
