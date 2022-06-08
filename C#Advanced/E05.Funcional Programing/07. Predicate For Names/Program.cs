aeb5d263912293eb3dca6f4d66cc92e6d37691b2
Grigor Petrunov <grigor.petrunov@gmail.com>
2022-06-08 10:09:29 +0300

    Added Solutions for 7, 10 and 11

---
+++ C#Advanced/E05.Funcional Programing/07. Predicate For Names/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace _07._Predicate_For_Names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            
+            List<string> names = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            List<string> newList = new List<string>();
+
+            Predicate<string> nameLenght = name => name.Length <= n;
+
+            foreach (var name in names)
+            {
+                if (nameLenght(name))
+                {
+                    newList.Add(name);
+                }
+                
+            }
+
+
+            foreach (var name in newList)
+            {
+                Console.WriteLine(name);
+            }
+                 
+
+
+        }
+    }
+}
