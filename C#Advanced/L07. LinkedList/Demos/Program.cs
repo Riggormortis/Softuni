eedd22e969624c953aa9ab16ffcebe77b3edb9dd
Grigor Petrunov <grigor.petrunov@gmail.com>
2022-06-11 09:48:35 +0300

    Added Demos and Simple Snake

---
+++ C#Advanced/L07. LinkedList/Demos/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Demos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DoublyLinkedList linked list = new
+
+
+
+                DoublyLinkedList linkedList = new DoublyLinkedList();
+
+            linkedList.AddFirst(new Node(1));
+            linkedList.AddFirst(new Node(2));
+            linkedList.AddFirst(new Node(3));
+            linkedList.AddFirst(new Node(4));
+
+            linkedList.ForEach(node =>
+            {
+                Console.WriteLine(node.Value);
+            });
+            Console.WriteLine("Reversed");
+            linkedList.Reverse();
+
+            linkedList.ForEach(node =>
+            {
+                Console.WriteLine(node.Value);
+            });
+            Console.WriteLine("Reversed");
+            linkedList.Reverse();
+
+            linkedList.ForEach(node =>
+            {
+                Console.WriteLine(node.Value);
+            });
+            Console.WriteLine("Reversed");
+            linkedList.Reverse();
+            foreach (var node in linkedList.ToArray())
+            {
+                Console.WriteLine("In Foreach");
+                Console.WriteLine(node.Value);
+            }
+        }
+    }
+}
