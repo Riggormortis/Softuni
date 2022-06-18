f82fa2712d8bb821b7874693cd55fb1f972fc95b
Grigor Petrunov <grigor.petrunov@gmail.com>
2022-06-18 23:57:06 +0300

    Added Solutions

---
+++ C#Advanced/Exams/C# Advanced Exam - 20 February 2021/02. Warships/Program.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._Warships
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+
+            int[] coordinates = Console.ReadLine()
+                .Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray();
+            
+            string[,] matrix = new string[n, n];
+
+            int firstPlayer = 0; //<
+            int secondPlayer = 0; //>
+            int totalShips = 0;
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                string[] input = Console.ReadLine().Split();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = input[col];
+
+                    if (matrix[row, col] == "<")
+                    {
+                        firstPlayer++;
+                    }
+                    else if (matrix[row, col] == ">")
+                    {
+                        secondPlayer++;
+                    }
+                }
+            }
+
+            totalShips = firstPlayer + secondPlayer;
+
+            for (int i = 0; i < coordinates.Length; i += 2)
+            {
+                int row = coordinates[i];
+                int col = coordinates[i + 1];
+
+                if (!IsInRange(matrix, row, col))
+                {
+                    continue;
+                }
+
+                if (matrix[row, col] == ">")
+                {
+                    secondPlayer--;
+                    matrix[row, col] = "X";
+                }
+                else if (matrix[row, col] == "<")
+                {
+                    firstPlayer--;
+                    matrix[row, col] = "X";
+                }
+                else if (matrix[row, col] == "#")
+                {
+                    //up
+                    if (IsInRange(matrix, row - 1, col))
+                    {
+                        if (matrix[row - 1, col] == ">")
+                        {
+                            secondPlayer--;
+                        }
+                        else if (matrix[row - 1, col] == "<")
+                        {
+                            firstPlayer--;
+                        }
+                        matrix[row - 1, col] = "X";
+                    }
+                    //down
+                    if (IsInRange(matrix, row + 1, col))
+                    {
+                        if (matrix[row + 1, col] == ">")
+                        {
+                            secondPlayer--;
+                        }
+                        else if (matrix[row + 1, col] == "<")
+                        {
+                            firstPlayer--;
+                        }
+                        matrix[row + 1, col] = "X";
+                    }
+                    //right/left
+                    //right
+                    if (IsInRange(matrix, row, col + 1))
+                    {
+                        if (matrix[row, col+1] == ">")
+                        {
+                            secondPlayer--;
+                        }
+                        else if (matrix[row, col + 1] == "<")
+                        {
+                            firstPlayer--;
+                        }
+                        matrix[row, col + 1] = "X";
+                    }
+                    //left
+                    if (IsInRange(matrix, row, col - 1))
+                    {
+                        if (matrix[row, col - 1] == ">")
+                        {
+                            secondPlayer--;
+                        }
+                        else if (matrix[row, col - 1] == "<")
+                        {
+                            firstPlayer--;
+                        }
+                        matrix[row, col - 1] = "X";
+                    }
+                    //up left/up right
+                    //up left
+                    if (IsInRange(matrix, row - 1, col - 1))
+                    {
+                        if (matrix[row - 1, col - 1] == ">")
+                        {
+                            secondPlayer--;
+                        }
+                        else if (matrix[row - 1, col - 1] == "<")
+                        {
+                            firstPlayer--;
+                        }
+                        matrix[row - 1, col - 1] = "X";
+                    }
+                    //up right
+                    if (IsInRange(matrix, row - 1, col + 1))
+                    {
+                        if (matrix[row - 1, col + 1] == ">")
+                        {
+                            secondPlayer--;
+                        }
+                        else if (matrix[row - 1, col + 1] == "<")
+                        {
+                            firstPlayer--;
+                        }
+                        matrix[row - 1, col + 1] = "X";
+                    }
+                    //down left/down right
+                    //down left
+                    if (IsInRange(matrix, row + 1, col - 1))
+                    {
+                        if (matrix[row + 1, col - 1] == ">")
+                        {
+                            secondPlayer--;
+                        }
+                        else if (matrix[row + 1, col - 1] == "<")
+                        {
+                            firstPlayer--;
+                        }
+                        matrix[row + 1, col - 1] = "X";
+                    }
+                    //down right
+                    if (IsInRange(matrix, row + 1, col + 1))
+                    {
+                        if (matrix[row + 1, col + 1] == ">")
+                        {
+                            secondPlayer--;
+                        }
+                        else if (matrix[row + 1, col + 1] == "<")
+                        {
+                            firstPlayer--;
+                        }
+                        matrix[row + 1, col + 1] = "X";
+                    }
+
+
+                }
+
+                if (firstPlayer == 0 || secondPlayer == 0)
+                {
+                    break;
+                }
+            }
+
+            if (firstPlayer > 0 && secondPlayer > 0)
+            {
+                Console.WriteLine($"It's a draw! Player One has {firstPlayer} ships left. Player Two has {secondPlayer} ships left.");
+            }
+            else if (firstPlayer > 0)
+            {
+                Console.WriteLine($"Player One has won the game! {totalShips - (firstPlayer + secondPlayer)} ships have been sunk in the battle.");
+            }
+            else if (secondPlayer > 0)
+            {
+                Console.WriteLine($"Player Two has won the game! {totalShips - (firstPlayer + secondPlayer)} ships have been sunk in the battle.");
+            }
+            
+
+        }
+
+        private static bool IsInRange(string[,] matrix, int row, int col)
+            => row >= 0 && row < matrix.GetLength(0)
+            && col >= 0 && col < matrix.GetLength(1);
+
+    }
+}
---
+++ C#Advanced/Exams/C# Advanced Retake Exam - 14 April 2021/01. Warm Winter/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Warm_Winter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // hats - stack
+            //scarfs - queue
+            int[] inputHats = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] inputScarfs = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+
+            Stack<int> hats = new Stack<int>(inputHats);
+            Queue<int> scarfs = new Queue<int>(inputScarfs);
+
+            List<int> elements = new List<int>();
+
+            //while elements.any
+            while (hats.Count > 0 && scarfs.Count > 0)
+            {
+                int hat = hats.Peek();
+                int scarf = scarfs.Peek();
+                //if (hats > scarfs) 
+                //-> net set -> hats + scarft
+                //remove hats and scarfs element
+                if (hat > scarf)
+                {
+                    elements.Add(hat + scarf);
+                    hats.Pop();
+                    scarfs.Dequeue();
+                }
+                //else if (scarfs > hats)
+                // remove hat
+                else if (scarf > hat)
+                {
+                    hats.Pop();
+                }
+                //else
+                //remove scarf
+                // element of hats + 1
+                else
+                {
+                    scarfs.Dequeue();
+                    int value = hats.Pop() + 1;
+                    hats.Push(value);
+                }
+               
+            }
+
+            Console.WriteLine($"The most expensive set is: {elements.Max()}");
+            Console.WriteLine(string.Join(" ", elements));
+
+        }
+    }
+}
