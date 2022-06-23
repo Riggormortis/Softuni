14c1e9c6e3e61005da4d908175dae80ce4535829
Grigor Petrunov <grigor.petrunov@gmail.com>
2022-06-23 23:56:21 +0300

    Add

---
+++ C#Advanced/Exams/C# Advanced Retake Exam - 13 April 2022/03. Zoo/Animal.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+
+namespace Zoo
+{
+    public class Animal
+    {
+        private string species;
+        private string diet;
+        private double weight;
+        private double length;
+
+        public string Species { get => species; private set { species = value; } }
+        public string Diet { get => diet; private set { diet = value; } }
+        public double Weight { get => weight; private set { weight = value; } }
+        public double Length { get => length; private set { length = value; } }
+
+        public Animal(string species, string diet, double weight, double length)
+        {
+            this.Species = species;
+            this.Diet = diet;
+            this.Weight = weight;
+            this.Length = length;
+        }
+
+        public override string ToString()
+        {
+            return $"The {this.Species} is a {this.Diet} and weighs {this.Weight} kg.";
+        }
+
+
+    }
+}
