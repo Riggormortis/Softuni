9c2782356e7f80c99b52d081a05b45a701e55282
Grigor Petrunov <grigor.petrunov@gmail.com>
2022-07-11 20:14:22 +0300

    add

---
+++ C#OOP/L04. Polymorphism/01. Math Operations/ICalculator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Operations
+{
+    public interface ICalculator
+    {
+        int Add(int a, int b);
+
+        double Add(double a, double b, double c);
+
+        decimal Add(decimal a, decimal b, decimal c);
+    }
+}
