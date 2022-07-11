5ff2b270dc007bc874302f6af66de4c03148a5ba
Grigor Petrunov <grigor.petrunov@gmail.com>
2022-07-11 20:42:47 +0300

    add and fix

---
+++ C#OOP/E03. Interfaces and Abstraction/05. Birthday Celebrations/80-100 - Fixed/Citizen.cs
@@ -0,0 +1,33 @@
+﻿using BorderControl.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BorderControl
+{
+    public class Citizen : IPerson, IIdentifiable
+    {
+       
+        public Citizen(string name, int age, string id, string birthdate)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Id = id;
+            this.Birthdate = birthdate;
+        }
+
+        public int Age { get; private set; }
+
+        public string Name { get; private set; }
+
+        public string Id { get; private set; }
+
+        public string Birthdate { get; private set; }
+
+        public override string ToString()
+        {
+            return $"{this.Birthdate}";
+        }
+
+    }
+}
---
+++ C#OOP/E03. Interfaces and Abstraction/05. Birthday Celebrations/80-100 - Fixed/Core/Engine.cs
@@ -0,0 +1,84 @@
+﻿using BorderControl.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BorderControl.Core
+{
+    public class Engine
+    {
+        private readonly List<IIdentifiable> identifiables;
+
+
+        public Engine()
+        {
+            this.identifiables = new List<IIdentifiable>();
+        }
+
+        public void Run()
+        {
+            string input = Console.ReadLine();
+
+            while (input != "End")
+            {
+                string[] inputTokens = input
+                    .Split()
+                    .ToArray();
+
+                if (inputTokens[0] == "Citizen")
+                {
+                    this.AddCitizen(inputTokens);
+                }
+                else if (inputTokens[0] == "Pet")
+                {
+                    this.AddPet(inputTokens);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            var birthdateAsked = Console.ReadLine();
+
+
+            if (identifiables.Count == 0)
+            {
+                Console.WriteLine("");
+            }
+            else
+            {
+                foreach (var identifiable in this.identifiables.Where(x => x.Birthdate.EndsWith(birthdateAsked)))
+                {
+                    Console.WriteLine(identifiable);
+                }
+            }
+            
+
+        }
+
+
+        private void AddCitizen(string[] inputTokens)
+        {
+            var name = inputTokens[1];
+            var age = int.Parse(inputTokens[2]);
+            var id = inputTokens[3];
+            var birthdate = inputTokens[4];
+
+            IIdentifiable citizen = new Citizen(name, age, id, birthdate);
+            this.identifiables.Add(citizen);
+        }
+
+        private void AddPet(string[] inputTokens)
+        {
+            var name = inputTokens[1];
+            var birthdate = inputTokens[2];
+            
+
+            IIdentifiable citizen = new Pet(name, birthdate);
+            this.identifiables.Add(citizen);
+        }
+
+        
+
+    }
+}
