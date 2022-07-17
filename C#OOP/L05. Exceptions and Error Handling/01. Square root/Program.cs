99570b4fe75f8f882a8e6ea78990fc1b6fb96b5e
Grigor Petrunov <grigor.petrunov@gmail.com>
2022-07-17 07:01:05 +0300

    add

---
+++ C#OOP/L05. Exceptions and Error Handling/01. Square root/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace SquareRoot
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            try
+            {
+
+                int number = int.Parse(Console.ReadLine());
+                if (number < 0)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                double result = Math.Sqrt(number);
+                Console.WriteLine(result);
+            }
+
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid number.");
+            }
+
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("Invalid number.");
+            }
+
+            finally
+            {
+                Console.WriteLine("Goodbye.");
+            }
+
+        }
+    }
+}
