e1725e093a2f72729ae0eeb99d7d82c47eedbd82
Grigor Petrunov <grigor.petrunov@gmail.com>
2022-07-24 21:29:22 +0300

    add

---
+++ C#OOP/L06. Reflection and Attributes/01. Stealer/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Stealer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+
+            Spy spy = new Spy();
+            string result = spy.StealFieldInfo("Stealer.Hacker", "username", "password");
+
+            Console.WriteLine(result);
+
+
+
+        }
+    }
+}
