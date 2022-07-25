44d7266aef191212aef8b164326c669c474ed9c6
Grigor Petrunov <grigor.petrunov@gmail.com>
2022-07-26 00:04:41 +0300

    add

---
+++ C#OOP/E06. Reflection and Attributes/01. Command Pattern/Core/Commands/ExitCommand.cs
@@ -0,0 +1,15 @@
+﻿namespace CommandPattern.Core.Commands
+{
+    using System;
+    using CommandPattern.Core.Contracts;
+    public class ExitCommand : ICommand
+    {
+        private const int SuccessExitCode = 0;
+        public string Execute(string[] args)
+        {
+            Environment.Exit(SuccessExitCode);
+
+            return null;
+        }
+    }
+}
