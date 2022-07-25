cacdad761a0fab1f9ea8f70858b41271835cad45
Grigor Petrunov <grigor.petrunov@gmail.com>
2022-07-25 20:28:14 +0300

    add

---
+++ C#OOP/L06. Reflection and Attributes/06. Code Tracker/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AuthorProblem
+{
+    [Author("Victor")]
+
+    public class StartUp
+    {
+        [Author("George")]
+        static void Main(string[] args)
+        {
+            var tracker = new Tracker();
+            tracker.PrintMethodsByAuthor();
+
+        }
+    }
+}
---
+++ C#OOP/L06. Reflection and Attributes/06. Code Tracker/Tracker.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Reflection;
+namespace AuthorProblem
+{
+    public class Tracker
+    {
+        public void PrintMethodsByAuthor()
+        {
+            Type type = typeof(StartUp);
+
+            foreach (var method in type.GetMethods(BindingFlags.Instance | BindingFlags.Public | BindingFlags.Static))
+            {
+                object[] attrs = method.GetCustomAttributes(false);
+
+                foreach (var attr in attrs)
+                {
+                    AuthorAttribute authorAttr = attr as AuthorAttribute;
+                    if (authorAttr != null)
+                    {
+                        Console.WriteLine($"{method.Name} is written by {authorAttr.Name}");
+                    }
+                }
+            }
+
+
+        }
+
+    }
+}
