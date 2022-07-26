78b1cf2da63301c5236f6931304655b4613bcd05
Grigor Petrunov <grigor.petrunov@gmail.com>
2022-07-26 21:07:20 +0300

    add

---
+++ C#OOP/E06. Reflection and Attributes/02. Validation Attributes/Models/Person.cs
@@ -0,0 +1,24 @@
+﻿namespace ValidationAttributes.Models
+{
+    using Utilities;
+    using ValidationAttributes.Utilities.Attributes;
+
+    public class Person
+    {
+        private const int AgeMinValue = 12;
+        private const int AgeMaxvalue = 90;
+
+
+        public Person(string fullName, int age)
+        {
+            this.FullName = fullName;
+            this.Age = age;
+        }
+
+        [MyRequired]
+        public string FullName { get; set; }
+
+        [MyRange(AgeMinValue, AgeMaxvalue)]
+        public int Age { get; set; }
+    }
+}
---
+++ C#OOP/E06. Reflection and Attributes/02. Validation Attributes/StartUp.cs
@@ -0,0 +1,22 @@
+﻿namespace ValidationAttributes
+{
+    using System;
+    using ValidationAttributes.Models;
+    using Utilities;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var person = new Person
+             (
+                 null, //"Pesho" <- Valid
+                 -1 //33 <- Valid
+             );
+
+            bool isValidEntity = Validator.IsValid(person);
+
+            Console.WriteLine(isValidEntity);
+        }
+    }
+}
