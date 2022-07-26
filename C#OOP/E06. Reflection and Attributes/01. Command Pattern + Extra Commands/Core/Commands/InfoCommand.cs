383e3434253e1137345b4d314e6a0f669c4b7125
Grigor Petrunov <grigor.petrunov@gmail.com>
2022-07-26 19:22:40 +0300

    add

---
+++ C#OOP/E06. Reflection and Attributes/01. Command Pattern + Extra Commands/Core/Commands/InfoCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommandPattern.Core.Commands
+{
+    using Contracts;
+
+    public class InfoCommand : ICommand
+    {
+        public string Execute(string[] args)
+        {
+            return $"My name is {args[0]} and I am {args[1]} years old!";
+        }
+    }
+}
