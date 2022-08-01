204c07e2d2b3c6bb8497269fe7531df4e4ce2d7a
Grigor Petrunov <grigor.petrunov@gmail.com>
2022-08-01 20:15:25 +0300

    add

---
+++ C#OOP/E07. Unit Testing/03. Car Manager/CarManager.Tests/CarManagerTests.cs
@@ -0,0 +1,116 @@
+namespace CarManager.Tests
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class CarManagerTests
+    {
+        private const double InitialFuelAmount = 0.0;
+
+        private Car car;
+
+        [SetUp]
+        public void Setup()
+        {
+            this.car = new Car("Make", "Model", 10.0, 100.0);
+        }
+
+        [Test]
+        [TestCase(null, "Model", 10.0, 100.0)]
+        [TestCase("", "Model", 10.0, 100.0)]
+        [TestCase("Make", null, 10.0, 100.0)]
+        [TestCase("Make", "", 10.0, 100.0)]
+        [TestCase("Make", "Model", 0, 100.0)]
+        [TestCase("Make", "Model", -10.0, 100.0)]
+        [TestCase("Make", "Model", 10.0, 0)]
+        [TestCase("Make", "Model", 10.0, -100.0)]
+        public void Ctor_ThrowsExceptionWhenAParameterIsInvalid (string make, string model, double fuelConsumption, double fuelCapacity)
+        {
+            Assert.That(() => new Car(make, model, fuelConsumption, fuelCapacity), Throws.ArgumentException);
+        }
+
+        [Test]
+        public void Ctor_SetsValuesToGivenParametersWhenValuesAreValid()
+        {
+            string make = "Make";
+            string model = "Model";
+            double fuelConsumption = 10.0;
+            double fuelCapacity = 100.0;
+
+            Car car = new Car(make, model, fuelConsumption, fuelCapacity);
+
+            Assert.That(car.Make, Is.EqualTo(make));
+            Assert.That(car.Model, Is.EqualTo(model));
+            Assert.That(car.FuelConsumption, Is.EqualTo(fuelConsumption));
+            Assert.That(car.FuelCapacity, Is.EqualTo(fuelCapacity));
+            Assert.That(car.FuelAmount, Is.EqualTo(InitialFuelAmount));
+        }
+
+        [Test]
+        [TestCase(0.0)]
+        [TestCase(-10.0)]
+        public void Refuel_ThrowsExceptionWhenGivenParameterIsEqualToOrLessThanZero(double fuelToRefuel)
+        {
+            Assert.That(() => this.car.Refuel(fuelToRefuel), Throws.ArgumentException, "Fuel amount cannot be zero or negative!");
+        }
+
+        [Test]
+        public void Refuel_IncreasesFuelAmount_WithGivenParameterWhenParameterIsValidAndThereIsEnoughFuelCapacity()
+        {
+            double fuelToRefuel = this.car.FuelCapacity / 2;
+
+            this.car.Refuel(fuelToRefuel);
+
+            Assert.That(this.car.FuelAmount, Is.EqualTo(fuelToRefuel));
+        }
+
+        [Test]
+        public void Refuel_IncreasesFuelAmountToFuelCapacityWhenGivenParameterIsValidButThereIsNotEnoughFuelCapacity()
+        {
+            double fuelToRefuel = this.car.FuelCapacity * 2;
+
+            this.car.Refuel(fuelToRefuel);
+
+            Assert.That(this.car.FuelAmount, Is.EqualTo(car.FuelCapacity));
+        }
+
+        [Test]
+        public void Drive_ThrowsExceptionWhenRequiredFuelIsMoreThanFuelAmount()
+        {
+            double fuelToRefuel = this.car.FuelCapacity / 2;
+            this.car.Refuel(fuelToRefuel);
+
+            double distance = (this.car.FuelAmount * this.car.FuelConsumption) + 1;
+
+            Assert.That(() => this.car.Drive(distance), Throws.InvalidOperationException, "You don't have enough fuel to drive!");
+        }
+
+        [Test]
+        public void Drive_ReducesFuelAmountToZeroWhenRequredFuelIsEqualToFuelAmount()
+        {
+            double fuelToRefuel = this.car.FuelCapacity / 2;
+            this.car.Refuel(fuelToRefuel);
+
+            double distance = (this.car.FuelAmount * this.car.FuelConsumption);
+
+            this.car.Drive(distance);
+
+            Assert.That(this.car.FuelAmount, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Drive_ReducesFuelAmountWithRequiredFuelWhenRequiredFuelIsLessThanFuelAmount()
+        {
+            double fuelToRefuel = this.car.FuelCapacity / 2;
+            this.car.Refuel(fuelToRefuel);
+            double fuelAmountBeforeDrive = this.car.FuelAmount;
+
+            double distance = (this.car.FuelAmount * this.car.FuelConsumption) / 2;
+
+            this.car.Drive(distance);
+            double fuelAmountAfterDrive = this.car.FuelAmount;
+
+            Assert.That(fuelAmountBeforeDrive, Is.EqualTo(fuelAmountAfterDrive + (distance * (this.car.FuelConsumption / 100))));
+        }
+    }
+}
