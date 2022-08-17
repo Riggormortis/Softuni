88c77a15a3b4cb7abc752aee4f98e3f35e15d272
Grigor Petrunov <grigor.petrunov@gmail.com>
2022-08-17 18:11:36 +0300

    add

---
+++ C#OOP/Exams/C# OOP Retake Exam - 22 August 2021/02. Business Logic/Models/Mission/Mission.cs
@@ -0,0 +1,36 @@
+﻿using SpaceStation.Models.Astronauts;
+using SpaceStation.Models.Astronauts.Contracts;
+using SpaceStation.Models.Mission.Contracts;
+using SpaceStation.Models.Planets.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpaceStation.Models.Mission
+{
+    public class Mission : IMission
+    {
+        public void Explore(IPlanet planet, ICollection<IAstronaut> astronauts)
+        {
+            
+            List<string> itemsOnPlanet = planet.Items.ToList();
+
+            foreach (var astronaut in astronauts.Where(a => a.CanBreath == true))
+            {
+                for (int i = 0; i < itemsOnPlanet.Count; i++)
+                {
+                    astronaut.Breath();
+                    astronaut.Bag.Items.Add(itemsOnPlanet[i]);
+                    itemsOnPlanet.RemoveAt(i);
+                    i--;
+
+                    if (!astronaut.CanBreath)
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
