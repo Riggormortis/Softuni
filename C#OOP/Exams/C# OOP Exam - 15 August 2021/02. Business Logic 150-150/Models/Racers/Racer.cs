12e78b93059b993e502d85ddc65068b2e0f45563
Grigor Petrunov <grigor.petrunov@gmail.com>
2022-08-19 19:01:07 +0300

    Added Repaired Solution

---
+++ C#OOP/Exams/C# OOP Exam - 15 August 2021/02. Business Logic 150-150/Models/Racers/Racer.cs
@@ -0,0 +1,110 @@
+﻿using CarRacing.Models.Cars.Contracts;
+using CarRacing.Models.Racers.Contracts;
+using CarRacing.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarRacing.Models.Racers
+{
+    public abstract class Racer : IRacer
+    {
+        private string username;
+        private string racingBehavior;
+        private int drivingExperience;
+        private ICar car;
+        
+
+        protected Racer(string username, string racingBehavior, int drivingExperience, ICar car)
+        {
+            this.Username = username;
+            this.RacingBehavior = racingBehavior;
+            this.DrivingExperience = drivingExperience;
+            this.Car = car;
+        }
+
+        public string Username
+        {
+            get => this.username;
+
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidRacerName);
+                }
+
+                this.username = value;
+            }
+        }
+
+        public string RacingBehavior
+        {
+            get => this.racingBehavior;
+
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidRacerBehavior);
+                }
+
+                this.racingBehavior = value;
+            }
+        }
+
+        public int DrivingExperience
+        {
+            get => this.drivingExperience;
+
+            protected set
+            {
+                if (value < 0 || value > 100)
+                {
+                    throw new ArgumentException(string.Format(ExceptionMessages.InvalidRacerDrivingExperience, value));
+                }
+
+                this.drivingExperience = value;
+            }
+        }
+
+        public ICar Car
+        {
+            get => this.car;
+
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidRacerCar);
+                }
+
+                this.car = value;
+            }
+        }
+
+        public virtual void Race()
+        {
+            car.Drive();
+            
+        }
+
+        public bool IsAvailable()
+        {
+           return this.Car.FuelAvailable >= this.Car.FuelConsumptionPerRace;
+        }
+
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{this.GetType().Name}: {this.Username}");
+            sb.AppendLine($"--Driving behavior: {this.RacingBehavior}");
+            sb.AppendLine($"--Driving experience: {this.DrivingExperience}");
+            sb.AppendLine($"--Car: {car.Make} {car.Model} ({car.VIN})");
+
+            return sb.ToString().TrimEnd();
+        }
+
+    }
+}
