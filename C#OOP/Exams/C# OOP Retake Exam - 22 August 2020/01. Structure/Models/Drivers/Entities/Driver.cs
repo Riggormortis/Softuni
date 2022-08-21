b8d6dc3e612a63d111370626e5a9a82fca510a31
Grigor Petrunov <grigor.petrunov@gmail.com>
2022-08-21 19:06:20 +0300

    add

---
+++ C#OOP/Exams/C# OOP Retake Exam - 22 August 2020/01. Structure/Models/Drivers/Entities/Driver.cs
@@ -0,0 +1,79 @@
+﻿using EasterRaces.Models.Cars.Contracts;
+using EasterRaces.Models.Drivers.Contracts;
+using EasterRaces.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasterRaces.Models.Drivers.Entities
+{
+    public class Driver : IDriver
+    {
+        private string name;
+        private int numberOfWins;
+        private double oxygen;
+        private ICar car;
+        private bool canParticipate;
+
+
+        public Driver(string name)
+        {
+            Name = name;
+        }
+
+
+        public string Name
+        {
+            get => name;
+
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value) || value.Length < 5)
+                {
+                    throw new ArgumentException(string.Format(ExceptionMessages.InvalidName));
+                }
+                this.name = value;
+            }
+        }
+
+        public ICar Car
+        {
+            get => car;
+            private set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentException(ExceptionMessages.CarInvalid);
+                }
+
+                car = value;
+            }
+        }
+
+        public int NumberOfWins
+        {
+            get => this.numberOfWins;
+            private set
+            {
+                this.numberOfWins = value;
+            }
+        }
+
+        public bool CanParticipate
+        {
+            get => canParticipate;
+            private set => canParticipate = value;
+        }
+
+        public void AddCar(ICar car)
+        {
+            Car = car;
+            CanParticipate = true;
+        }
+
+        public void WinRace()
+        {
+            NumberOfWins++;
+        }
+    }
+}
---
+++ C#OOP/Exams/C# OOP Retake Exam - 22 August 2020/01. Structure/Repositories/Entities/Repository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EasterRaces.Repositories.Contracts;
+
+namespace EasterRaces.Repositories.Entities
+{
+    public abstract class Repository<T> : IRepository<T> where T : class
+    {
+        private List<T> models;
+
+        protected Repository()
+        {
+            models = new List<T>();
+        }
+        public List<T> Models => models;
+       
+        public void Add(T model)
+        {
+            models.Add(model);
+        }
+
+        public bool Remove(T model)
+        {
+            return models.Remove(model);
+        }
+
+        public abstract T GetByName(string name);
+
+        public IReadOnlyCollection<T> GetAll()
+        {
+            return models as IReadOnlyCollection<T>;
+        }
+
+       
+
+        
+    }
+}
